cd7588012586736d82301edc0d266de630f42550
Din Music <din.music@canonical.com>
2024-12-20 15:34:43 +0000

    internal/*: Configure standalone precheks
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/storage/resource_storage_volume_test.go
+++ internal/storage/resource_storage_volume_test.go
@@ -13,7 +13,10 @@ func TestAccStorageVolume_basic(t *testing.T) {
 	volumeName := acctest.GenerateName(2, "-")
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckStandalone(t)
+		},
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
@@ -37,7 +40,10 @@ func TestAccStorageVolume_instanceAttach(t *testing.T) {
 	volumeName := acctest.GenerateName(2, "-")
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckStandalone(t)
+		},
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
@@ -109,7 +115,10 @@ func TestAccStorageVolume_contentType(t *testing.T) {
 	volumeName := acctest.GenerateName(2, "-")
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckStandalone(t)
+		},
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
@@ -155,7 +164,10 @@ func TestAccStorageVolume_importProject(t *testing.T) {
 	resourceName := "lxd_volume.volume1"
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckStandalone(t)
+		},
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
@@ -177,7 +189,10 @@ func TestAccStorageVolume_inheritedStoragePoolKeys(t *testing.T) {
 	volumeName := acctest.GenerateName(2, "-")
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
+		PreCheck: func() {
+			acctest.PreCheck(t)
+			acctest.PreCheckStandalone(t)
+		},
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
