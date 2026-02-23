4e9a0ff30a1e5d290da3c16908e9cc0af2dca5df
Din Music <din.music@canonical.com>
2026-02-23 12:42:43 +0000

    auth: Allow importing identity
    Signed-off-by: Din Music <din.music@canonical.com>

--- internal/auth/resource_identity.go
+++ internal/auth/resource_identity.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
@@ -286,4 +287,30 @@ func (r AuthIdentityResource) SyncState(ctx context.Context, tfState *tfsdk.Stat
 	m.Groups = groups
 
 	return tfState.Set(ctx, &m)
+}
+
+func (r *AuthIdentityResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	meta := common.ImportMetadata{
+		ResourceName:   "auth_identity",
+		RequiredFields: []string{"auth_method", "name"},
+	}
+
+	fields, diag := meta.ParseImportID(req.ID)
+	if diag != nil {
+		resp.Diagnostics.Append(diag)
+		return
+	}
+
+	for k, v := range fields {
+		// Attribute "project" is parsed by default, but is not allowed for auth identity.
+		if k == "project" {
+			resp.Diagnostics.AddError(
+				fmt.Sprintf("Invalid import ID %q", req.ID),
+				"Valid import format:\nimport lxd_auth_identity.<resource> [remote:]<auth_method>/<name>",
+			)
+			break
+		}
+
+		resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root(k), v)...)
+	}
 }
